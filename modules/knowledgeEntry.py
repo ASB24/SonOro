a469de168ee432f576eb8d273d5e0abfed13f44e
Alex Serafini <alexserafini2001@gmail.com>
2023-03-29 01:48:06 -0400

    write genres to file in order to keep consistent with dataset.dat

--- modules/knowledgeEntry.py
+++ modules/knowledgeEntry.py
@@ -4,54 +4,82 @@ from typing import List
 from pydub import AudioSegment
 import pickle
 from helpers import extractFeature
+from collections import defaultdict
 
 
-def createDataset(datasetPath: str, genresPath: str) -> bool:
+def createDataset(datasetPath: str, genresPath: str, genresFilePath: str) -> bool:
     if os.path.exists(datasetPath):
         os.remove(datasetPath)
-        
-    with open(datasetPath, 'wb') as f:
+    if os.path.exists(genresFilePath):
+        os.remove(genresFilePath)
+
+    genres = defaultdict(int)
+
+    with open(datasetPath, 'wb') as datasetFile:
         i = 0
         path = ''
         for folder in os.listdir(genresPath):
             i += 1
-            print(folder)
+            genres[i] = folder
+            print(f'Processing {genres[i]}...')
             for file in os.listdir(f'{genresPath}/{folder}'):
                 try:
                     path = f'{genresPath}/{folder}/{file}'
                     if not file.endswith(".wav"):
                         path = convertToWav(path)
-                    
+
                     audio = AudioSegment.from_file(path)
 
-                    target_loudness = 15
+                    target_loudness = 10
                     normalized_audio = audio.normalize(target_loudness)
                     normalized_audio.export(path, format="wav")
-                        
-                    feature = extractFeature(path, i)
-                    
-                    pickle.dump(feature, f)
-                    
+                    try:
+                        insertToDataset(datasetFile, path, i)
+                    except Exception as e:
+                        print(e)
+                        datasetFile.close()
+
                 except EOFError:
-                    f.close()
+                    datasetFile.close()
                 except Exception as e:
                     print(e)
                     print(f'Error encountered while parsing file: {file}')
-                    f.close()
+                    datasetFile.close()
                     return False
+
+    print('Writing genres to file...')
+    with open(genresFilePath, 'wb') as genresFile:
+        pickle.dump(genres, genresFile)
+
     return True
+
+
+def insertToDataset(datasetFile: str, filePath: str, iterations: int) -> bool:
+    try:
+        feature = extractFeature(filePath, iterations)
+        pickle.dump(feature, datasetFile)
+    except EOFError:
+        datasetFile.close()
+        return False
+    except Exception as e:
+        print(e)
+        datasetFile.close()
+        return False
+    return True
+
 
 def convertToWav(filePath: str) -> str:
     sound = AudioSegment.from_file(filePath)
-    
+
     os.remove(filePath)
-    
+
     path = '.'.join(filePath.split('.')[:-1])
     wavPath = f'{path}.wav'
-    
+
     sound = sound.export(wavPath, format='wav')
     sound = AudioSegment.from_file(wavPath)
     return wavPath
+
 
 def loadDatasets(datasetPath: str, split: float) -> List:
     dataset = []
